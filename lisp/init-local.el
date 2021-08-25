22e1e5aa4d5be2e6467c124725e9c6d20b88e06a
Santiago Díez <sdiez@tenkeys.es>
2021-08-25 23:36:20 +0200

    Added web-mode for Vue files

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,3 +1,5 @@
+(require 'use-package)
+
 ;; Custom key bindings
 (setq mac-option-key-is-meta nil)
 (setq mac-command-key-is-meta t)
@@ -8,5 +10,20 @@
 ;; If loaded from Finder move default dir to ~
 (if window-system
     (setq default-directory "~/"))
+
+;; Vue
+;; We are going to use web-mode
+;; TODO: Explore lsp
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 
 (provide 'init-local)
